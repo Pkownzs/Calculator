fe3aae072ede71657f9b6e28ca01668df2106ca0
Pkownzs <pkownzs@gmail.com>
2023-05-28 11:11:54 +0300

    third commit

--- calculator.py
+++ calculator.py
@@ -1,4 +1,19 @@
 #Simple Calculator maybe if i will have free time, will try to make it more realistic with real calculator that would have more functionality
+
+# to add 
+#Decimal Precision: 
+# By allowing the user to specify the desired decimal precision, 
+# you provide more flexibility in the calculator's output. 
+# You can add an option for the user to input the desired precision (e.g., the number of decimal places) 
+# and use Python's round() function to round the calculated results to the specified precision.
+#Memory Functionality: 
+# Adding memory functionality allows users to store and recall values during multiple calculations. 
+# You can include options such as "M+" (add to memory), "M-" (subtract from memory), "MR" (recall from memory), or "MC" (clear memory). 
+# This can be useful when performing calculations that depend on previous results, eliminating the need for the user to re-enter values.
+#Refractor code:
+#Refractor the code so it would be more easy to read it, and it would be closer looking as a real calculator with basic functionality.
+#Remake this calculator so it would work from opening caculator.exe file and it would have an interface similar as a calculator with basic funcionality.
+
 import math
 import sys
 
@@ -21,19 +36,20 @@ while True:
     print("10. cos - Cosine")
     print("11. tan - Tangent")
     print("0. Exit program")
-        
+
     z = input("Operation that you want to excecute: ")
-
-
         
     if z == "1" or z == "+":
         try:
             x = float(input("First number: "))
             y = float(input("Second number: "))
-        except ValueError:
+        except (ValueError, NameError):
             print(f"ValueError: Your input is not a valid number")
+            continue
+        
         result = x + y
         print(f"Result after + operation: {x} + {y} = {result}")
+        
         while True:
             print("If you want to repeat operation write 1 or word repeat or again.\nIf you want to exit write exit or number 2")
             excecute = input("Wirte operation that you would like to excecute from shown meniu above: ")
@@ -43,8 +59,9 @@ while True:
                 try:
                     x = float(input("First number:" ))
                     y = float(input("Second number: "))
-                except ValueError:
+                except (ValueError, NameError):
                     print(f"ValueError: Your input is not a valid number")
+                    continue
                 
                 result = x + y
                 print(f"Result after + operation: {x} + {y} = {result}")
@@ -59,23 +76,48 @@ while True:
         try:
             x = float(input("First number: "))
             y = float(input("Second number: "))
-        except ValueError:
+        except (ValueError,NameError):
             print(f"ValueError: Your input is not a valid number")
+            continue
+        
         result = x - y
         print(f"Result after - operation: {x} - {y} = {result}")
+
+        while True:
+            print("If you want to repeat operation write 1 or word repeat or again.\nIf you want to exit write exit or number 2")
+            excecute = input("Wirte operation that you would like to excecute from shown meniu above: ")
+            
+            if excecute == "1" or excecute.lower() == "repeat" or excecute.lower() == "again":
+                
+                try:
+                    x = float(input("First number:" ))
+                    y = float(input("Second number: "))
+                except (ValueError, NameError):
+                    print(f"ValueError: Your input is not a valid number")
+                    continue
+                
+                result = x - y
+                print(f"Result after - operation: {x} - {y} = {result}")
+                
+            elif excecute == "2" or excecute.lower() == "exit":
+                print("Thanks for using the simple calculator, have a nice day")
+                # exit_loop = True
+                sys.exit()
+                # break
 
 
     if z == "3" or z== "/":
         try:
             x = float(input("First number: "))
             y = float(input("Second number: "))
-        except ValueError:
+        except (ValueError,NameError):
             print(f"ValueError: Your input is not a valid number")
             continue
+        
         # except ZeroDivisionError:
         #     print(f"ValueError: Your input is not a valid number") #Not working at the moment
-        if x == 0 or y == 0:
-            print("Error it cant be divided with zero")
+        # if x == 0 or y == 0:
+        #     print("Error it cant be divided with zero")
         
         result = x / y
         print(f"Result after / operation: {x} / {y} = {result}")
@@ -85,8 +127,10 @@ while True:
         try:
             x = float(input("First number: "))
             y = float(input("Second number: "))
-        except ValueError:
+        except (ValueError,NameError):
             print(f"ValueError: Your input is not a valid number")
+            continue
+        
         result = x * y
         print(f"Result after * operation: {x} * {y} = {result}")
 
@@ -94,8 +138,10 @@ while True:
         try:
             x = float(input("First number: "))
             y = float(input("Second number: "))
-        except ValueError:
+        except (ValueError,NameError):
             print(f"ValueError: Your input is not a valid number")
+            continue
+        
         result = x // y
         print(f"Result after // operation: {x} // {y} = {result}")
         
@@ -103,8 +149,10 @@ while True:
         try:
             x = float(input("First number: "))
             y = float(input("Second number: "))
-        except ValueError:
+        except (ValueError,NameError):
             print(f"ValueError: Your input is not a valid number")
+            continue
+        
         result = x ** y
         print(f"Result after ** operation: {x} ** {y} = {result}")
         
@@ -112,44 +160,53 @@ while True:
         try:
             x = float(input("First number: "))
             y = float(input("Second number: "))
-        except ValueError:
+        except (ValueError,NameError):
             print(f"ValueError: Your input is not a valid number")
+            continue
+        
         result = x % y
         print(f"Result after % operation: {x} % {y} = {result}")
         
     if z == "8" or z.lower() == "sqrt" or z.lower() == "square root" or z.lower() == "root" or z.lower() == "square":
         try:
-            x = float(input("First number: "))
-        except ValueError:
+            x = float(input("Your number: "))
+        except (ValueError,NameError):
             print(f"ValueError: Your input is not a valid number")
+            continue
+        
         result = math.sqrt(x)
         print(f"Result after square root operation: Square Root: {x} = {result}")
         
     if z == "9" or z.lower() == "sin" or z.lower() == "sine":
         try:
-            x = float(input("First number: "))
-        except ValueError:
+            x = float(input("Your number: "))
+        except (ValueError,NameError):
             print(f"ValueError: Your input is not a valid number")
+            continue
+        
         result = math.sin(x)
         print(f"Result after sine operation: sine: {x} = {result}")
     
     if z == "10" or z.lower() == "cos" or z.lower() == "cosine":
         try:
-            x = float(input("First number: "))
-        except ValueError:
+            x = float(input("Your number: "))
+        except (ValueError,NameError):
             print(f"ValueError: Your input is not a valid number")
-        result = math.sin(x)
+            continue
+        
+        result = math.cos(x)
         print(f"Result after cosine operation: cosine: {x} = {result}")
         
     if z == "11" or z.lower() == "tan" or z.lower() == "tangent":
         try:
-            x = float(input("First number: "))
-        except ValueError:
+            x = float(input("Your number: "))
+        except (ValueError,NameError):
             print(f"ValueError: Your input is not a valid number")
-        result = math.sin(x)
+            continue
+        
+        result = math.tan(x)
         print(f"Result after tangent operation: tangent: {x} = {result}")
         
     if z == "0" or z.lower() == "exit":
         print("Thanks for using the simple calculator, have a nice day")
-        exit_loop = True
         break
